a26a67acc4d117398ead40445a0fa9d405d8c24e
alexa <alexandr1lvl@gmail.com>
2026-03-22 21:39:04 +0300

    add postgresql support

--- corporate_lms/corporate_lms/settings.py
+++ corporate_lms/corporate_lms/settings.py
@@ -63,8 +63,12 @@ WSGI_APPLICATION = 'corporate_lms.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('DB_NAME', str(BASE_DIR / 'db.sqlite3')),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
